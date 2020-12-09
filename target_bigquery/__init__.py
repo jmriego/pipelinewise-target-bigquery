618111cd8817ee4c6b5303b62f8602845a09aeef
Jorge Luis Juarez <jorge.juarez@casai.com>
2020-12-09 14:08:08 -0600

    Update __init__.py

--- target_bigquery/__init__.py
+++ target_bigquery/__init__.py
@@ -181,7 +181,12 @@ def persist_lines(config, lines) -> None:
 
             # Validate record
             try:
-                validators[stream].validate(float_to_decimal(o['record']))
+                validate = float_to_decimal(o['record'])
+                logger.info("============= 1 ===============")
+                logger.info(validate)
+                logger.info("============= 2 ===============")
+                validators[stream].validate(validate)
+                logger.info("============= 3 ===============")
             except Exception as ex:
                 if type(ex).__name__ == "InvalidOperation":
                     logger.error("Data validation failed and cannot load to destination. RECORD: {}\n'multipleOf' validations that allows long precisions are not supported (i.e. with 15 digits or more). Try removing 'multipleOf' methods from JSON schema."
